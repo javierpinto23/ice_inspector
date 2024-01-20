6888d4a5575bb9bd35bf76a55befdda83a870cfd
Javier Pinto <javier.pinto@adidas.com>
2024-01-20 14:27:53 -0500

    ICE-INSP-first_artificial_network

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8n.pt')
 
-cap = cv2.VideoCapture("/Users/pintojav/Library/CloudStorage/OneDrive-adidas/MASTER_CLASSES_OSCAR-JAIME/CLASS_HOW_ADD_NEW_FIELD_CONNECTOOR_KAFKA.mp4")
+cap = cv2.VideoCapture("./test_video.mp4")
 
 ret = True
 
 while ret:
     ret, frame = cap.read()
-    results = model.track(frame, persist=True)
 
-    frame = frame[0].plot()
+    if ret:
 
-    cv2.imshow('frame', frame_)
+        results = model.track(frame, persist=True)
 
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
+        frame_ = results[0].plot()
+
+        cv2.imshow('frame', frame_)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+
 
